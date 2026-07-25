175d435d1a81d0ee25362c4daa658670b793fca1
Manoj Manivannan <manojm18@live.in>
2026-07-25 21:39:19 +0100

    Update Rofi themes to use Catppuccin color scheme

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -19,9 +19,9 @@ hl.monitor({
 local terminal = "alacritty"
 local fileManager = "nautilus"
 local browser = "firefox"
--- Uses the vendored adi1090x type-2/style-1 launcher theme (tokyonight colors).
+-- Uses the vendored adi1090x type-2/style-2 launcher theme (catppuccin colors).
 -- Try other styles with: rofi -show drun -theme ~/.config/rofi/launchers/type-N/style-M.rasi
-local menu = "rofi -show drun -theme $HOME/.config/rofi/launchers/type-2/style-1.rasi"
+local menu = "rofi -show drun -theme $HOME/.config/rofi/launchers/type-3/style-4.rasi"
 local reload_waybar = "pkill waybar; waybar &"
 local snip = "grimblast --notify copysave area"
 
