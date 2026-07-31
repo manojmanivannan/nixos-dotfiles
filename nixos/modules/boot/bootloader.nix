8d9e7f53122bba904bca4bbd2217b2316a552820
Manoj Manivannan <manojm18@live.in>
2026-07-31 14:25:19 +0100

    retain only last 10 generations

--- nixos/modules/boot/bootloader.nix
+++ nixos/modules/boot/bootloader.nix
@@ -4,7 +4,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.timeout = 5;
   boot.initrd.enable = true;
   boot.initrd.verbose = false;
